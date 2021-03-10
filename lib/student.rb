f8ffecccd5fc844cffd3d5c350c32c3d5ac7b29f
rheadp <rheadpea@msu.edu>
2021-03-10 12:36:11 -0500

    commit tree 0c0d8c78ee41a4610827462daf82c96945c352dd

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,59 @@
 require_relative "../config/environment.rb"
-
 class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
+  def initialize(name, grade, id= nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def update
+    DB[:conn].execute("UPDATE students SET name = ?, grade = ? WHERE id = ?", @name, @grade, @id)
+  end
+  
+  def save
+    if @id
+      update
+    else
+      DB[:conn].execute("INSERT INTO students(name, grade) VALUES(?,?)", @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+  end
+
+  def self.new_from_db(row)
+    id, name, grade = row
+    self.new(name, grade, id)
+  end
+
+  def self.find_by_name(name)
+    student_hash = DB[:conn].execute("SELECT * FROM  students WHERE name = ? LIMIT 1", name)[0]
+    self.new_from_db(student_hash)
+  end
 end
